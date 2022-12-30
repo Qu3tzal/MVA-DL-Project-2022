9947555896499a81770c5042be02985f204f3c12
Maxime Alvarez <maxime.alvarez@usherbrooke.ca>
2022-12-30 13:32:10 +0900

    Slightly changed dataset loading helpers

--- datasets.py
+++ datasets.py
@@ -8,18 +8,18 @@ AVAILABLE_DATASETS = ['MS-COCO', 'Flickr-8k']
 def load_dataset(dataset_name, dirpath):
     if dataset_name == "MS-COCO":
         return get_ms_coco_dataset(dirpath)
-    elif dataset_name == "Flickr-8k"
+    elif dataset_name == "Flickr-8k":
         return get_flickr8k_dataset(dirpath)
 
 def get_ms_coco_dataset(dirpath):
     ds = CocoCaptions(root=dirpath + '/images',
-                    annFile=dirpath + '/annotations.txt',
+                    annFile=dirpath + '/captions.json',
                     transform=PILToTensor())
     return ds
 
 
 def get_flickr8k_dataset(dirpath):
     ds = Flickr8k(root=dirpath + '/images',
-               annFile=dirpath + '/annotations.txt',
+               annFile=dirpath + '/captions.json',
                transform=PILToTensor())
     return ds
