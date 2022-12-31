3c366f0ea9e90f46929c3885e09f699c04c8ea29
Maxime Alvarez <maxime.alvarez@usherbrooke.ca>
2022-12-31 14:47:13 +0900

    Added some preprocessing to datasets

--- datasets.py
+++ datasets.py
@@ -1,25 +1,41 @@
-from torchvision.transforms import PILToTensor
+from torchvision.transforms import Compose, PILToTensor, ToTensor, Resize
 from torchvision.datasets import CocoCaptions, Flickr8k
+import query_embedder as qe
 
 
 AVAILABLE_DATASETS = ['MS-COCO', 'Flickr-8k']
 
 
-def load_dataset(dataset_name, dirpath):
+def load_dataset(dataset_name, dirpath, embedder):
     if dataset_name == "MS-COCO":
-        return get_ms_coco_dataset(dirpath)
+        return get_ms_coco_dataset(dirpath, embedder)
     elif dataset_name == "Flickr-8k":
-        return get_flickr8k_dataset(dirpath)
+        return get_flickr8k_dataset(dirpath, embedder)
 
-def get_ms_coco_dataset(dirpath):
+
+def get_ms_coco_dataset(dirpath, embedder):
+    def target_transform_fn(targets):
+        # TODO: use all the captions, not just one.
+        encoded_caption = embedder.encode(targets[0])
+        return encoded_caption
+
     ds = CocoCaptions(root=dirpath + '/images',
                     annFile=dirpath + '/captions.json',
-                    transform=PILToTensor())
+                    target_transform=target_transform_fn,
+                    transform=Compose([
+                        Resize((224, 224)),
+                        ToTensor()
+                    ])
+                )
     return ds
 
 
-def get_flickr8k_dataset(dirpath):
+def get_flickr8k_dataset(dirpath, embedder):
     ds = Flickr8k(root=dirpath + '/images',
                annFile=dirpath + '/captions.json',
-               transform=PILToTensor())
+               transform=Compose([
+                   Resize((224, 224)),
+                   ToTensor()
+               ])
+            )
     return ds
