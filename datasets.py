890600036b0b7c2cd1170ec8fbf7088e2fedd720
Maxime Alvarez <maxime.alvarez@usherbrooke.ca>
2023-01-03 22:51:54 +0900

    Added Flickr loading class

--- datasets.py
+++ datasets.py
@@ -1,5 +1,6 @@
+import PIL.Image
 from torchvision.transforms import Compose, PILToTensor, ToTensor, Resize
-from torchvision.datasets import CocoCaptions, Flickr8k
+from torchvision.datasets import CocoCaptions, VisionDataset
 import query_embedder as qe
 
 
@@ -30,9 +31,48 @@ def get_ms_coco_dataset(dirpath, embedder):
     return ds
 
 
+class CustomFlickr8kDataset(VisionDataset):
+    def __init__(self,
+                 root,
+                 annotations_file_filepath,
+                 transform=None,
+                 target_transform=None) -> None:
+        if root[-1] != '/':
+            root = root + '/'
+        super().__init__(root=root, transform=transform, target_transform=target_transform)
+        self.ids = [
+
+        ]
+        self.targets = dict()
+
+        with open(annotations_file_filepath, 'rt') as annotations_file:
+            lines = annotations_file.readlines()
+
+            for line in lines:
+                name_id = line.split("#")[0]
+                target = line.split("#")[1]
+                self.ids.append(name_id)
+                self.targets[name_id] = self.targets.get(name_id, [target])
+
+    def __getitem__(self, index: int):
+        img = PIL.Image.open(self.root + self.ids[index])
+        targets = self.targets[self.ids[index]]
+
+        return self.transform(img), self.target_transform(targets)
+
+    def __len__(self) -> int:
+        return len(self.ids)
+
+
 def get_flickr8k_dataset(dirpath, embedder):
-    ds = Flickr8k(root=dirpath + '/images',
-               annFile=dirpath + '/captions.json',
+    def target_transform_fn(targets):
+        # TODO: use all the captions, not just one.
+        encoded_caption = embedder.encode(targets[0])
+        return encoded_caption
+
+    ds = CustomFlickr8kDataset(root=dirpath + '/images',
+               annotations_file_filepath=dirpath + '/captions.txt',
+               target_transform=target_transform_fn,
                transform=Compose([
                    Resize((224, 224)),
                    ToTensor()
