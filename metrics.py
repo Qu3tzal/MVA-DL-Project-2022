0fa9690678feebd6ca4efd93ad3275d7981927ba
Maxime Alvarez <maxime.alvarez@usherbrooke.ca>
2023-01-08 10:41:02 +0100

    Added BLEU metric

--- metrics.py
+++ metrics.py
@@ -1,13 +1,50 @@
 import pycocotools as cc
 
+from nltk.translate.bleu_score import sentence_bleu
 
 AVAILABLE_METRICS = ["BLEU", "CIDER", "METEOR"]
 
 
 def get_fn(fn_name):
     if fn_name == "BLEU":
-        return None
+        return compute_bleu
     elif fn_name == "CIDER":
-        return None
+        return compute_cider
     elif fn_name == "METEOR":
-        return None
+        return compute_meteor
+
+
+def compute_bleu(query: list[str], caption: list[str]):
+    """
+    Computes the BLEU score between the query and the caption.
+    Args:
+        query: the tokenized query
+        caption: the tokenized caption
+    Return:
+        the BLEU score
+    """
+    return sentence_bleu([query], caption)
+
+
+def compute_cider(query: list[str], caption: list[str]):
+    """
+    Computes the CIDEr score between the query and the caption.
+    Args:
+        query: the tokenized query
+        caption: the tokenized caption
+    Return:
+        the CIDEr score
+    """
+    pass
+
+
+def compute_meteor(query: list[str], caption: list[str]):
+    """
+    Computes the METEOR score between the query and the caption.
+    Args:
+        query: the tokenized query
+        caption: the tokenized caption
+    Return:
+        the METEOR score
+    """
+    pass
